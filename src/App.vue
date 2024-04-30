d8b02dbc3acbab9e3ea3ee6e3e6b1e6c79dad483
Big-Cake-jpg <2577117205@qq.com>
2024-04-30 19:48:34 +0800

    Add Vercel Speed Insights

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import Loading from './components/Loading.vue'
 import { onMounted, ref } from 'vue';
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <template>
+  <SpeedInsights />
   <Header />
   <BackToTop />
   <div class="p-4 max-w-screen-md m-auto font-sans">
